fbb89664cdf15e314ee8e2618558dd51a201d044
MorganSlimak <morganslimak@gmail.com>
2015-10-27 17:20:13 -0400

    added search term functionality and timeout

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 var app = angular.module("youtubeApp", []);
 
 var apiKey = "AIzaSyD3pT_2BEZ4StGCB3BP5W8rmDypJ-sbiBI";
-var youtube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=news&maxResults=12&order=date";
+var searchText = "news";
 
-app.controller("MainController", ["$scope", "$http", function($scope, $http) {
-	$http({
-    url: youtube + "&key=" + apiKey + "&callback=JSON_CALLBACK",
-    method: "jsonp"
-  }).success(function(data, status) {
-    $scope.videos = data.items;
-  });
+app.controller("VideoController", ["$scope", "$http", "$timeout", function($scope, $http, $timeout) {
+
+ $scope.$watch("search", function(search) {
+ 	var time;
+	if (search != undefined) { searchText = search; time = 1000; }
+	else time = 0;
+	var youtube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchText + "&maxResults=12";
+	$timeout(function() {
+		$http({
+	    url: youtube + "&key=" + apiKey + "&callback=JSON_CALLBACK",
+	    method: "jsonp"
+	  }).success(function(data, status) {
+	    $scope.videos = data.items;
+		})
+	}, time);
+});
+
+
 }]);
